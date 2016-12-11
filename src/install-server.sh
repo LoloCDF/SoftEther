f2076df36e969edcb44b1c18262ae31753a44fa1
Manuel Aragón Añino <maragon.contacto@gmail.com>
2016-12-11 14:41:45 +0100

    Added 'restore-default-service-conf.sh' script.

--- src/install-server.sh
+++ src/install-server.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+# Control variables
+INSTALLED=FALSE
+ROOT=FALSE
+
+# We get the current user
 user=$(whoami)
 
+# We have to check if we are root
 if [ "$user" != "root" ]; then
 	echo "In order to install correctly, you need to install as root user."
 else
+	ROOT=TRUE
+fi
+
+# We have to check if the server is already installed
+ls /usr/local/vpnserver &> /dev/null
+
+if [ $? -eq 0 ]; then
+	echo "Server already installed."
+	INSTALLED=TRUE
+fi
+
+if [ $INSTALLED = "FALSE" ] && [ $ROOT = "TRUE" ]; then	
 	echo "Decompressing SoftEther Server..."
 	tar xzvf softether-vpnserver-v4.22-9634-beta-2016.11.27-linux-x64-64bit.tar.gz
 	echo "Installing..."
@@ -16,4 +34,12 @@ else
 	export PATH="$PATH:/usr/local/vpnserver/"
 	echo $PATH	
 	echo "SoftEther Server installed in /usr/local/, you can check it running 'vpncmd' command."	
+	echo "Setting up the first configuration..."
+	cp -f ./files/vpnserver /etc/init.d
+	chmod 755 /etc/init.d/vpnserver
+	/sbin/chkconfig --add vpnserver
+	service vpnserver start
+	service vpnserver stop
+	echo "You can now start the service."
+	cp /usr/local/vpnserver/vpn_server.config /usr/local/vpnserver/vpn_server.config.bck
 fi
--- src/uninstall-server.sh
+++ src/uninstall-server.sh
@@ -1,10 +1,27 @@
+# We have to check if you are running as root user
+ROOT=FALSE
 user=$(whoami)
 
 if [ "$user" != "root" ]; then
 	echo "In order to uninstall correctly, you need to run this script as root user."
 else
+	ROOT=TRUE
+fi
+
+# We have to check sever is installed
+INSTALLED=FALSE
+
+ls /usr/local/vpnserver &> /dev/null
+
+if [ $? -eq 0 ]; then
+	INSTALLED=TRUE
+else
+	echo "SoftEther VPN Server is not installed."
+fi
+
+if [ $ROOT = "TRUE" ] && [ $INSTALLED = "TRUE" ]; then	
 	PATHBCK=$(cat /usr/local/vpnserver/PATH.bck)
 	export PATH="$PATHBCK"
-	rm -rf /usr/local/vpnserver
+	rm -rf /usr/local/vpnserver /etc/init.d/vpnserver
 	echo "SoftEther VPN Server has been uninstalled correctly."
 fi
